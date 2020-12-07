970b6b3b92cb668bc73f74fbc859796705b5aa1b
AdamWil <Adam6457@gmail.com>
2020-12-07 14:16:06 -0700

    Added mats to vive contorller prefab and player prefab
    Keyboard does not accept inputs, either system or bluetooth.
    
    HL2 Good

---
+++ Assets/Adam_Assets/Scripts/General/tmp/SpawnTextInputPhoton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
+using Photon.Utilities;
+
+namespace Photon_IATK
+{
+
+    public class SpawnTextInputPhoton : MonoBehaviourPunCallbacks
+    {
+        public GameObject myPrefab;
+        public void spawnTextInput()
+        {
+            if (PhotonNetwork.IsConnected)
+                {
+                if (!photonView.IsMine)
+                {
+                    PhotonNetwork.Instantiate("MRTK_InputFeild", new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+                    Debug.Log(GlobalVariables.green + "Connected, Spawning a text input feild" + GlobalVariables.endColor + " : " + this.GetType());
+                }
+             } else
+            {
+                Instantiate(myPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            }
+        }
+
+    }
+}
