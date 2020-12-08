476a0524b234ff53addb92e6f409ccafda086d13
AdamWil <Adam6457@gmail.com>
2020-12-08 16:05:48 -0700

    Added lobby scripts (unsused)
    HL2_

---
+++ Assets/Adam_Assets/Scripts/PhotonManagement/testing/Room_Async.cs
@@ -0,0 +1,103 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+
+namespace Photon_IATK { 
+    public class Room_Async : MonoBehaviourPunCallbacks, IInRoomCallbacks
+
+    { 
+    public static Room_Async Room;
+
+    [SerializeField] private GameObject photonUserPrefab = default;
+
+    // private PhotonView pv;
+    private Player[] photonPlayers;
+    private int playersInRoom;
+    private int myNumberInRoom;
+
+    // private GameObject module;
+    // private Vector3 moduleLocation = Vector3.zero;
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        base.OnPlayerEnteredRoom(newPlayer);
+        photonPlayers = PhotonNetwork.PlayerList;
+        playersInRoom++;
+    }
+
+    private void Awake()
+    {
+        if (Room == null)
+        {
+            Room = this;
+        }
+        else
+        {
+            if (Room != this)
+            {
+                Destroy(Room.gameObject);
+                Room = this;
+            }
+        }
+    }
+
+    public override void OnEnable()
+    {
+        base.OnEnable();
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    public override void OnDisable()
+    {
+        base.OnDisable();
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+
+    private void Start()
+    {
+        // Allow prefabs not in a Resources folder
+        if (PhotonNetwork.PrefabPool is DefaultPool pool)
+        {
+            if (photonUserPrefab != null) pool.ResourceCache.Add(photonUserPrefab.name, photonUserPrefab);
+        }
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+
+        photonPlayers = PhotonNetwork.PlayerList;
+        playersInRoom = photonPlayers.Length;
+        myNumberInRoom = playersInRoom;
+        PhotonNetwork.NickName = myNumberInRoom.ToString();
+
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        CreatPlayer();
+        if (!PhotonNetwork.IsMasterClient) return;
+    }
+
+    private void CreatPlayer()
+    {
+        var player = PhotonNetwork.Instantiate(photonUserPrefab.name, Vector3.zero, Quaternion.identity);
+    }
+
+            // private void CreateMainLunarModule()
+            // {
+            //     module = PhotonNetwork.Instantiate(roverExplorerPrefab.name, Vector3.zero, Quaternion.identity);
+            //     pv.RPC("Rpc_SetModuleParent", RpcTarget.AllBuffered);
+            // }
+            //
+            // [PunRPC]
+            // private void Rpc_SetModuleParent()
+            // {
+            //     Debug.Log("Rpc_SetModuleParent- RPC Called");
+            //     module.transform.parent = TableAnchor.Instance.transform;
+            //     module.transform.localPosition = moduleLocation;
+            // }
+    }
+}
---
+++ Assets/Adam_Assets/Scripts/Vive/OpenVR_Pose_Tracking.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using Microsoft.MixedReality.Toolkit;
+
+namespace Photon_IATK
+{
+
+    // this script updates the attached objects transfrom to match a controllers transform
+    public class OpenVR_Pose_Tracking : ControllerPoseSynchronizer
+    {
+        public Handedness Handedness;
+        private new IMixedRealityController Controller;
+
+        public override void OnInputChanged(InputEventData<MixedRealityPose> eventData)
+        {
+            if (eventData.SourceId == Controller?.InputSource.SourceId)
+            {
+                    TrackingState = TrackingState.Tracked;
+                    transform.position = eventData.InputData.Position;
+                    transform.rotation = eventData.InputData.Rotation;
+            }
+        }
+
+        public override void OnSourceDetected(SourceStateEventData eventData)
+        {
+            Debug.Log("OnSourceDetected: Attemping to find controller");
+            if (eventData.Controller.ControllerHandedness == Handedness)
+            {
+                Controller = eventData.Controller;
+                Debug.Log("OnSourceDetected: Tracking - " + GlobalVariables.green + Handedness + " controller" + GlobalVariables.endColor + " : ID=" + Controller.InputSource.SourceId + " : Name= " + Controller.InputSource.SourceName);
+            }
+
+        }
+    }
+}
+
+//== eventData.MixedRealityInputAction
