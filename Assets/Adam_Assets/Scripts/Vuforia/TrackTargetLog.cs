79d6781439e9fa1f676d037813f000cef960c23d
AdamWil <Adam6457@gmail.com>
2020-12-12 08:41:05 -0700

    Vuforia settings tweaked
    HL2 G

---
+++ Assets/Adam_Assets/Scripts/Vuforia/TrackTargetLog.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackTargetLog : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    GameObject TrackedObj;
+    bool isTracking = false;
+
+    public void onTargetFound()
+    {
+        Debug.Log("Target Found" + this.GetType());
+        TrackedObj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        TrackedObj.transform.localScale = new Vector3(0.079f, 0.049f, 0.0849f);
+        TrackedObj.transform.position = this.transform.position;
+        TrackedObj.transform.rotation = this.transform.rotation;
+        isTracking = true;
+
+    }
+
+    public void onTargetLost()
+    {
+        Debug.Log("Target Lost" + this.GetType());
+        Destroy(TrackedObj);
+        isTracking = false;
+    }
+
+    private void Update()
+    {
+        if (isTracking)
+        {
+            TrackedObj.transform.position = this.transform.position;
+            TrackedObj.transform.rotation = this.transform.rotation;
+        }
+    }
+
+
+}
