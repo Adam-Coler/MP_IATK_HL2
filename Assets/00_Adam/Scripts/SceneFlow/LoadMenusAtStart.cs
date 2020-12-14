a3804df5cb3b5195bd2f75d6c95c974b7d15d898
AdamWil <Adam6457@gmail.com>
2020-12-14 11:06:29 -0700

    Logs cause error on load
    Fixed menu load script with 2 second delay
    HL2 G

--- Assets/00_Adam/Scripts/SceneFlow/LoadMenusAtStart.cs
+++ Assets/00_Adam/Scripts/SceneFlow/LoadMenusAtStart.cs
@@ -8,13 +8,24 @@ namespace Photon_IATK {
         // Start is called before the first frame update
         void Start()
         {
+            StartCoroutine(LoadMenus());
+        }
+
+        IEnumerator LoadMenus()
+        {
+            yield return new WaitForSeconds(2);
+
+            
             GameObject Menu = GameObject.FindGameObjectWithTag("Menu");
             if (Menu == null)
             {
+                Debug.Log(GlobalVariables.green + "Loading menu :" + GlobalVariables.endColor + "LoadMenus() : " + this.GetType());
                 Btn_Functions_For_In_Scene_Scripts Btns = new Btn_Functions_For_In_Scene_Scripts();
                 Debug.Log(GlobalVariables.green + "Loading Main Menu " + GlobalVariables.endColor + " : " + "Start()" + " : " + this.GetType());
                 Btns.sceneManager_Load_01_SetupMenu();
             }
+
+            Destroy(this);
         }
 
     }
