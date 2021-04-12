a717f027739e574101f9a14b9b9b63d6725c5499
AdamWil <Adam6457@gmail.com>
2021-04-12 15:24:40 -0600

    pre mean plane

---
+++ Assets/00_Adam/Scripts/Annotations/MeanPlane.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Photon_IATK
+{
+    public class MeanPlane : MonoBehaviour
+    {
+        public GameObject plane;
+        public enum axisSelection { xAxis, yAxis, zAxis };
+        public axisSelection currentAxis = axisSelection.zAxis;
+
+
+
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
