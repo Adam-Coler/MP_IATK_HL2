1669864ab40f9782896150c6573200a9b387a859
AdamWil <Adam6457@gmail.com>
2021-08-31 13:29:01 -0600

    Vis moves independent to spawn and trash

---
+++ Assets/00_Adam/Scripts/Controls/UnParentReparent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Photon_IATK
+{
+    [DisallowMultipleComponent]
+    public class UnParentReparent : MonoBehaviour
+    {
+        private Transform myParent;
+        void Awake()
+        {
+            myParent = this.transform.parent;
+        }
+
+        public void deParent()
+        {
+            if (myParent == null) return;
+            this.transform.parent = null;
+        }
+
+        public void reParent()
+        {
+            if (myParent == null) return;
+            this.transform.parent = myParent;
+        }
+
+    }
+}
